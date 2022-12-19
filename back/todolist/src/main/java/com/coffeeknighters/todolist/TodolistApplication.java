160023e2f806ba3778646ea874bf027b9cec1f8a
OTalDoL2 <lucas.ramos_dev@outlook.com>
2022-12-19 08:37:54 -0300

    MVP Frontend

--- back/todolist/src/main/java/com/coffeeknighters/todolist/TodolistApplication.java
+++ back/todolist/src/main/java/com/coffeeknighters/todolist/TodolistApplication.java
@@ -2,25 +2,10 @@ package com.coffeeknighters.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
 public class TodolistApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
 	}
-
-	@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOriginPatterns("http://localhost:8082/").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS",  "HEAD", "TRACE", "CONNECT");;
-            }
-        };
-    }
-
 }
